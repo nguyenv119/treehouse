c11d5a3d90a8c65749ae494a4b6685b0744e3760
Ming-Shih <minstone@gmail.com>
2023-10-08 16:34:24 -0400

    update space

--- frontend/src/pages/space.tsx
+++ frontend/src/pages/space.tsx
@@ -1,30 +1,62 @@
 import styles from './space.module.css';
 import Draggable from 'react-draggable';
 import FormDialog from './FormDialog';
+import { useState } from 'react';
 
 export default function Space() {
+
+	interface Note {
+		topic: string;
+		note: string;
+		id: string;
+	}
+
+	const [notes, setNotes] = useState<Array<Note>>([]);
+
+	// const cards = [
+	// 	{
+	// 		topic: 'hello',
+	// 		note: 'world',
+	// 		id: crypto.randomUUID()
+	// 	},
+	// 	{
+	// 		topic: 'hello',
+	// 		note: 'world',
+	// 		id: crypto.randomUUID()
+	// 	},
+	// 	{
+	// 		topic: 'hello',
+	// 		note: 'world',
+	// 		id: crypto.randomUUID()
+	// 	}
+	// ];
+
+	function handleSubmit(event: React.FormEvent<HTMLFormElement>, topic: string, note: string) {
+		event.preventDefault();
+		setNotes([...notes, {
+			topic: topic,
+			note: note,
+			id: crypto.randomUUID()
+		}]);
+	}
+
 	return (
 		<>
 
 			<div>
 				<h1>Space</h1>
-				<Draggable >
-					<div className={styles.notes}>
-						hey
-					</div>
-				</Draggable >
-				<Draggable >
-					<div className={styles.notes}>
-						hey
-					</div>
-				</Draggable >
-				<Draggable >
-					<div className={styles.notes}>
-						hey
-					</div>
-				</Draggable >
+				{
+					notes.map((card) => (
+						<Draggable key={card.id}>
+							<div className={styles.notes}>
+								<p>{card.topic}</p>
+								<p>{card.note}</p>
+							</div>
+						</Draggable >
+					))
+				}
 			</div >
-			<FormDialog />
+			<FormDialog handleSubmit={handleSubmit} />
 		</>
 	)
 }
