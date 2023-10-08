b3ab16c47cb4d3f62aad7233bedc2bcd33b9e5ca
Long Nguyen <nguyenv@brandeis.edu>
2023-10-08 16:38:24 -0400

    change bg

--- ML/Models/chatBot.py
+++ ML/Models/chatBot.py
@@ -1,5 +1,4 @@
 import openai
-import gradio
 
 openai.api_key = "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"
 
@@ -42,17 +41,3 @@ while True:
     messages.append({"role": "assistant", "content": assistant_message})
     
     print("Assistant: " + assistant_message)
-
-
-# def CustomChatGPT(user_input):
-#     messages.append({"role": "user", "content": user_input})
-#     response = openai.ChatCompletion.create(
-#         model = "gpt-3.5-turbo",
-#         messages = messages
-#     )
-#     ChatGPT_reply = response["choices"][0]["message"]["content"]
-#     messages.append({"role": "assistant", "content": ChatGPT_reply})
-#     return ChatGPT_reply
-
-# demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Real Estate Pro")
-# demo.launch(share=True)
