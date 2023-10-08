45751bcede87ebfe41ac1f98e7372e2deab75ba4
Zachary Szeto <zacharyszto@gmail.com>
2023-10-08 11:27:59 -0400

    Info Section

--- frontend/src/components/Cards.tsx
+++ frontend/src/components/Cards.tsx
@@ -1,5 +1,13 @@
 import styles from './Cards.module.css';
 import { useNavigate } from 'react-router-dom';
+import { FaGraduationCap } from 'react-icons/fa';
+import { BiSolidBed } from 'react-icons/bi';
+import { AiFillHome } from 'react-icons/ai';
+import { FaUserFriends } from 'react-icons/fa';
+import { GiSuitcase } from 'react-icons/gi';
+import { BsQuestionLg } from 'react-icons/bs';
+
+
 
 export default function Cards() {
 	const navigate = useNavigate();
@@ -15,39 +23,38 @@ export default function Cards() {
 					<h1 className={styles.title}>Welcome to Spaces :)</h1>
 					<div className={styles.serviceList}>
 						<div className={styles.box1} onClick={handleClick}>
-							{/* <i className="fa-solid fa-car"></i> */}
-							<h2>Topic</h2>
-							<p>Question</p>
+							<h2><FaGraduationCap className="icon" /> Classes</h2>
+							<p>"Anyone else not understand anything from CS1101 today? It feels like I am the only one not getting it."</p>
 							<a href="">Reply</a>
 						</div>
 						<div className={styles.box2}>
 							{/* <i className="fa-solid fa-car"></i> */}
-							<h2>Topic</h2>
-							<p>Question</p>
+							<h2><BiSolidBed className="icon" /> Roomates</h2>
+							<p>"Am I falling behind when my roommate is already in Calc 4 while I'm still working on Calc 1?"</p>
 							<a href="">Reply</a>
 						</div>
 						<div className={styles.box3}>
 							{/* <i className="fa-solid fa-car"></i> */}
-							<h2>Topic</h2>
-							<p>Question</p>
+							<h2><AiFillHome className="icon" /> Family</h2>
+							<p>"My sister always gets better grades than me, which makes me feel like my parents are more proud of her than they are of me, and I wonder if I'm just not as smart as her."</p>
 							<a href="">Reply</a>
 						</div>
 						<div className={styles.box4}>
 							{/* <i className="fa-solid fa-car"></i> */}
-							<h2>Topic</h2>
-							<p>Question</p>
+							<h2><FaUserFriends className="icon" /> Social</h2>
+							<p>"It's the first week of school, and I already see people with so many friends, but I feel like I don't know anyone."</p>
 							<a href="">Reply</a>
 						</div>
 						<div className={styles.box5}>
 							{/* <i className="fa-solid fa-car"></i> */}
-							<h2>Topic</h2>
-							<p>Question</p>
+							<h2><GiSuitcase className="icon" /> Jobs/Internships</h2>
+							<p>"I see everyone getting internships and jobs on LinkedIn, but I haven't even gotten an interview yet."</p>
 							<a href="">Reply</a>
 						</div>
 						<div className={styles.box6}>
 							{/* <i className="fa-solid fa-car"></i> */}
-							<h2>Topic</h2>
-							<p>Question</p>
+							<h2><BsQuestionLg className="icon" /> Other</h2>
+							<p>""</p>
 							<a href="">Reply</a>
 						</div>
 
--- frontend/src/components/Info.tsx
+++ frontend/src/components/Info.tsx
@@ -1,15 +1,43 @@
-import styles from './Info.module.css'
+import styles from './Info.module.css';
 
 export default function Info() {
-	return (
-		<>
-			<div className={styles.section} id='info'>
-				<div className={styles.container}>
-					<h1>Initiative</h1>
-					<p>Impostor syndrome is a psychological pattern where individuals doubt their abilities and fear being exposed as frauds, despite their accomplishments. It was first described by psychologists Clance and Imes in a 1978 paper. Since then, numerous studies and resources have explored this phenomenon, including books like "The Secret Thoughts of Successful Women" by Valerie Young and "The Impostor Phenomenon" by Clance. TED Talks and Valerie Young's website also offer valuable insights and strategies to overcome it. Impostor syndrome can affect anyone and is characterized by self-doubt and attributing success to external factors, rather than one's own abilities. Recognizing it is the first step toward building self-confidence.</p>
-				</div>
-			</div >
+  return (
+    <>
+	<h1 className={styles.title}>Welcome to Info :)</h1>	
+      
+	  <div className={styles.container}>
+        <div className={styles.section}>
+        	<h2>The Problem: Imposter Syndrome</h2>
+          	<p><br></br>Imposter syndrome is the persistent feeling of self-doubt and insecurity about one's abilities, despite evidence of competence. Studies show up to 70% of college students experience it (A). Imposter syndrome exacerbates anxiety, depression, and burnout in students by causing them to set unrealistically high expectations, downplay their skills, and avoid asking for help (B). This disproportionately impacts groups underrepresented in higher education (C). By providing an anonymous forum for students to find community and work through self-doubts without judgment, we aim to address the detrimental effects of imposter syndrome on student wellbeing and achievement (D).
 
-		</>
-	)
+			  <br></br>
+			  <br></br>
+			Sources:<br></br>
+				A: (Canning et al., 2020; Peteet et al., 2015)<br></br>
+				B: (Chae et al., 1995; Serge et al., 2021)<br></br>
+				C: (Cokley et al., 2013; Peteet et al., 2015)<br></br>
+				D: (Serge et al., 2021; Vaishnav, 2020)
+</p>
+        </div>
+        <div className={styles.section}>
+          	<h2>Our Mission</h2>
+          	<p><br></br>Our mission is to create a safe and supportive online community for students struggling with imposter syndrome. By providing an anonymous forum, we aim to reduce the isolation and stigma students face when they don't understand course material or assignments. Our chatbot, powered by OpenAI, will facilitate constructive discussions where students can openly share their vulnerabilities without judgment. Moderation will prevent harmful or malicious content. Together, through open communication and compassion, we will empower students to overcome self-doubt and achieve their academic potential.</p>
+        </div>
+        <div className={styles.section}>
+          	<h2>Stastics</h2>
+          	<p>
+			  	• Up to 70% of college students experience feelings of imposter syndrome at some point during their academic careers (Canning et al., 2020; Peteet et al., 2015).
+				<br></br>
+				<br></br>
+				• In one study of over 1,700 undergraduate students, imposter syndrome was positively correlated with higher levels of anxiety, depression, psychological distress and burnout (Serge et al., 2021).
+				<br></br>
+				<br></br>
+				• College students experiencing imposter syndrome are more likely to set unattainably high standards for their academic performance, with 65% expecting nothing less than perfection (Chae et al., 1995).
+				<br></br>
+				<br></br>
+				• In a study of over 600 graduate students, 45% of those experiencing imposter syndrome reported feeling too ashamed or embarrassed to seek academic assistance when struggling (Peteet et al., 2015).</p>
+        </div>
+      </div>
+    </>
+  );
 }
