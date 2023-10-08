207c0899eee87e63a5f924809b63d763a5eda46a
Zachary Szeto <zacharyszto@gmail.com>
2023-10-07 20:22:22 -0400

    Fixed Margins

--- sase-hackathon/src/components/Card.tsx
+++ sase-hackathon/src/components/Card.tsx
@@ -1,31 +1,44 @@
-import styles from './Card.module.css'
+import styles from './Card.module.css';
 
 export default function Card() {
 	return (
 		<>
-			<div className={styles.container}>
+			<div className={styles.container} id="services">
+    			<div className={styles.serviceList}>
+					
+					
 
-				<div className={styles.serviceList}>
-
 					<div className={styles.box1}>
 						{/* <i className="fa-solid fa-car"></i> */}
 						<h2>Topic</h2>
 						<p>Question</p>
 						<a href="">Reply</a>
 					</div>
-					<div className={styles.box1}>
+					<div className={styles.box2}>
 						{/* <i className="fa-solid fa-car"></i> */}
 						<h2>Topic</h2>
 						<p>Question</p>
 						<a href="">Reply</a>
 					</div>
-					<div className={styles.box1}>
+					<div className={styles.box3}>
 						{/* <i className="fa-solid fa-car"></i> */}
 						<h2>Topic</h2>
 						<p>Question</p>
 						<a href="">Reply</a>
 					</div>
-					<div className={styles.box1}>
+					<div className={styles.box4}>
+						{/* <i className="fa-solid fa-car"></i> */}
+						<h2>Topic</h2>
+						<p>Question</p>
+						<a href="">Reply</a>
+					</div>
+					<div className={styles.box5}>
+						{/* <i className="fa-solid fa-car"></i> */}
+						<h2>Topic</h2>
+						<p>Question</p>
+						<a href="">Reply</a>
+					</div>
+					<div className={styles.box6}>
 						{/* <i className="fa-solid fa-car"></i> */}
 						<h2>Topic</h2>
 						<p>Question</p>
